fa31e20135b62887234bb8932ea0ce4796d4d82f
Summer <gasawase@mail.uc.edu>
2019-12-10 16:39:50 -0500

    worked on logic still click still not working

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,17 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-var randNum = Math.round(Math.random() * 10);
-
 function onLoad () {
-    document.getElementById('guessButt').addEventListener("click", runProg());
+    // document.getElementById('guessButt').addEventListener("click", runProg());
+    document.getElementById("guessButt").onclick = function() {runProg()};//works for browser
 }
 
- function runProg (){
+ function runProg ()
+ {
+    var randNum = Math.round(Math.random() * 10);
     console.log("Button pressed");
-    var theirNum = document.getElementsById("userGuess");
+    var theirNum = document.getElementsById("userGuess"); ///CONVERT TO NUM OR INT I THINK IT'S READING AS STRING
+    
 
     //exception handling
-    if (theirNum >= 0 && theirNum <= 10){
+    if (theirNum >= 0 || theirNum <= 10){
 
         document.getElementById("compNum").innerHTML = randNum;
         document.getElementById("userGuess").innerHTML = userGuess;
